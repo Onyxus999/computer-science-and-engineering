533ffecb551a3c3af149c1d9885e6ea90341bab8
Rogers-Boyer Family <rogers-boyer@rogersboyer-Predator-G3610>
2015-10-07 10:37:09 -0700

    Modified credit.c. Seems to work only with AMEX. Will do more research before continuing.

--- coursework/cs50x3/pset1/credit.c
+++ coursework/cs50x3/pset1/credit.c
@@ -82,12 +82,22 @@ int main(void) {
 		i = 0;
 		int j = len_cred_num-2;
 		int k = 0;
-		int len_sum = (len_cred_num/2)/*+1*/;
-		int sum[len_sum];
+		int len_sum;
+
+		if ((len_cred_num % 2) == 0) {
+			len_sum = (len_cred_num/2)+1;
+
+		}
+		else {
+			len_sum = (len_cred_num/2);
+
+		}
+
+		int lst_sum[len_sum];
 
 		for (i = 0; i < len_cred_num; i += 2) {
 			//printf("The %d digit is %d\n", j, lst_cred_num[j]);
-			sum[k] += (lst_cred_num[j]*2);
+			lst_sum[k] = (lst_cred_num[j]*2);
 			j -= 2;
 			k++;
 
@@ -95,7 +105,113 @@ int main(void) {
 		
 		i = 0;
 		for (i = 0; i < len_sum; i++) {
-			printf("[%d] ", sum[i]);
+			printf("[%d] ", lst_sum[i]);
+
+		}
+
+		printf("\n");
+
+		printf("The digits to sum are: ");
+
+		int lst_len_sum[len_sum+1];
+
+		i = len_sum+1;
+		j = 0;
+		for (i = len_sum+1; i > 0; i--) {
+			lst_len_sum[i] = count_digl(lst_sum[i-1]);
+			j += count_digl(lst_sum[i-1]);
+
+		}
+
+		lst_len_sum[0] = j;
+
+		i = 0;
+		j = 0;
+		k = 0;
+		int lst_sum_dig[lst_len_sum[0]];
+
+		printf("[");
+
+		for (i = 0; i < len_sum; i++) {
+			for (j = 0; j < lst_len_sum[i+1]; j++) {
+				lst_sum_dig[k] = digital(lst_sum[i], j);
+				if (i == len_sum-1) {
+					printf("%d]", lst_sum_dig[k]);
+				
+				}
+				else {
+					printf("%d ", lst_sum_dig[k]);
+
+				}
+				k++;
+
+			}
+
+		}
+
+		printf("\n");
+
+		printf("The sum of the digits is: ");
+
+		i = 0;
+		int sum = 0;
+
+		for (i = 0; i < lst_len_sum[0]-1; i++) {
+			//printf("\nSummed Digit: %d\n", lst_sum_dig[i]);
+			sum += lst_sum_dig[i];
+			//printf("Sum: %d\n", sum);
+
+		}		
+		
+		printf("%d\n", sum);
+
+		printf("The list of other numbers to be summed: ");
+	
+		i = 0;
+		j = len_cred_num-1;
+		k = 0;
+		int lst_other_dig[len_cred_num-len_sum];
+
+		for (i = 0; i < len_cred_num; i += 2) {
+			//printf("The %d digit is %d\n", j, lst_cred_num[j]);
+			lst_other_dig[k] = lst_cred_num[j];
+			sum += lst_cred_num[j];
+			j -= 2;
+			k++;
+
+		}
+		
+		i = 0;
+		for (i = 0; i < len_sum; i++) {
+			printf("[%d] ", lst_other_dig[i]);
+
+		}
+
+		printf("\n");
+		
+		printf("The total sum is: %d\n", sum);
+
+		if (digital(sum, count_digl(sum)-1) == 0) {
+			if (lst_cred_num[0] == 4) {
+				printf("VISA\n");
+
+			}
+			if (lst_cred_num[0] == 5) {
+				printf("MASTERCARD\n");
+
+			}
+			if (len_cred_num == 15) {
+				printf("AMEX\n");
+
+			}
+			else {
+				printf("INVALID\n");
+			
+			}
+
+		}
+		else {
+			printf("Not Valid!\n");
 
 		}
 
