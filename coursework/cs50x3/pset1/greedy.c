0f52eef791b0d7f2cb18731639fbe937962fb9ff
Onyxus999 <self.onyxus@gmail.com>
2015-10-15 04:25:01 -0400

    Finished greedy.c

--- coursework/cs50x3/pset1/greedy.c
+++ coursework/cs50x3/pset1/greedy.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main(void)
 {
-	int money;
+	int money = -1;
 	int number_of_coins = 0;
-
-	printf("Enter what you would like change for: ");
-	money = GetFloat()*100;
-
-	while (money >= 25)
+    
+    while (money < 0)
+    {
+        printf("Enter what you would like change for: ");
+	    money = roundf(GetFloat()*100);
+    }
+	
+    while (money >= 25)
 	{
 		money -= 25;
 		number_of_coins++;
 	}
-	while (money >= 10)
+	
+    while (money >= 10)
 	{
 		money -= 10;
 		number_of_coins++;
 	}
-	while (money >= 5)
+	
+    while (money >= 5)
 	{
 		money -= 5;
 		number_of_coins++;
 	}
-	while (money >= 1)
+	
+    while (money >= 1)
 	{
 		money -= 1;
 		number_of_coins++;
 	}
-	printf("You'd need %d coins to make that change.\n", number_of_coins);	
-
+    
+    printf("%d\n", number_of_coins);
 	return 0;
+
 }
