a5227ebfe6d06f1f0cedf6f42677b3f54899c995
Rogers-Boyer Family <rogers-boyer@rogersboyer-Predator-G3610>
2015-10-15 23:56:09 -0700

    Finished commenting all source code

--- coursework/cs50x3/pset2/caesar.c
+++ coursework/cs50x3/pset2/caesar.c
@@ -7,15 +7,18 @@
 #define UPALPHA "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #define LOALPHA "abcdefghijklmnopqrstuvwxyz"
 
+/*	This function tests to see if a string contains only digits
+ *
+ */
 int isnum(char* test_str)
 {
 	int is_number = 1;
 	for (int i = 0; i < strlen(test_str); i++)
 	{
-		if (!isalnum(test_str[i]))
-			is_number = 0;
-		else if (isalpha(test_str[i]))
-			is_number = 0;
+		if (!isalnum(test_str[i])) // if it contains a non-alphanumeric char
+			is_number = 0; // its not a number
+		else if (isalpha(test_str[i])) // if it contains a letter
+			is_number = 0; // its not a number
 	}
 	
 	return is_number;
@@ -23,45 +26,48 @@ int isnum(char* test_str)
 
 int main(int argc, char* argv[])
 {
-	if (argc == 1 || argc > 2)
+	if (argc == 1 || argc > 2) // if there is none or more than 1 argument
 	{
 		printf("caesar:: Error: Must have exactly one argument\n");
-		return 1;
+		return 1; // return error
 	}
-	else if (!isnum(argv[1]))
+	else if (!isnum(argv[1])) // if the argument isn't a number
 	{
 		printf("caesar:: Error: Please input an integer.\n");
-		return 2;
+		return 2; // return error 2
 	}
 	else
 	{
-		int key = atoi(argv[1]);
-		printf("Enter text to encrypt: ");
-		char* plaintext = GetString();
+		int key = atoi(argv[1]); // get integer from command line as key
+		//printf("Enter text to encrypt: ");
+		char* plaintext = GetString(); // get plaintext from user
 		
-		int* indexes = malloc(sizeof(int) * strlen(plaintext));
+		// this will be a list of indicies of the plaintext
+		// i.e. if the plaintext is abc this will be 0, 1, 2
+		int* indexes = malloc(sizeof(int) * strlen(plaintext)); 
 		for (int i = 0; i < strlen(plaintext); i++)
 		{
 			if (isalpha(plaintext[i]))
 			{
 				indexes[i] = (plaintext[i] % 32) - 1;
 			}
-			else
-				indexes[i] = 42;
+			else // if the plaintext has a non-alphanumeric char in it
+				indexes[i] = 42; // the index is 42 (or anything > 25)
 		}
 	
+		// this will be the string that holds the cyphertext
 		char* cyphertext = malloc(sizeof(char) * strlen(plaintext));
-		
 		for (int i = 0; i < strlen(plaintext); i++)
 		{
-			if (indexes[i] > 25)
-				cyphertext[i] = plaintext[i];
+			if (indexes[i] > 25) // if the index is > 25
+				// its non-alphanumeric
+				cyphertext[i] = plaintext[i]; // so ignore it
 			else
 			{
-				if (isupper(plaintext[i]))
-					cyphertext[i] = UPALPHA[(indexes[i] + key) % 26];
-				else
-					cyphertext[i] = LOALPHA[(indexes[i] + key) % 26];
+				if (isupper(plaintext[i])) // if its uppercase
+					cyphertext[i] = UPALPHA[(indexes[i] + key) % 26]; // put the ((p_i + k) % 26)th letter of the uppercase alphabet there
+				else // if its lowercase
+					cyphertext[i] = LOALPHA[(indexes[i] + key) % 26]; // put the ((p_i + k) % 26)th letter of the lowercase alphabet there
 			}
 
 		}		
--- coursework/cs50x3/pset2/vigenere.c
+++ coursework/cs50x3/pset2/vigenere.c
@@ -7,15 +7,18 @@
 #define UPALPHA "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #define LOALPHA "abcdefghijklmnopqrstuvwxyz"
 
+/*	This function checks if a string is comprised only of
+ *	letters.
+ */
 int isstring(char* test_str)
 {
         int is_string = 1;
         for (int i = 0; i < strlen(test_str); i++)
         {
-                if (!isalnum(test_str[i]))
-                        is_string = 0;
-                else if (isdigit(test_str[i]))
-                        is_string = 0;
+                if (!isalnum(test_str[i])) // if it has non-alphanumeric chars 
+                        is_string = 0; // its not all letters
+                else if (isdigit(test_str[i])) // if it has numbers
+                        is_string = 0; // its not all letters
         }
 
         return is_string;
@@ -23,22 +26,24 @@ int isstring(char* test_str)
 
 int main(int argc, char* argv[])
 {
-	if (argc == 1 || argc > 2)
+	if (argc == 1 || argc > 2) // if there is more than exactly one argument
 	{
 		printf("vigenere:: Error: Must have exactly one argument\n");
-		return 1;	
+		return 1; // error
 	}
-	else if (!isstring(argv[1]))
+	else if (!isstring(argv[1])) // if the argunent isn't only letters
 	{
 		printf("vigenere:: Error: Key must be alphabetical\n");
-		return 1;
+		return 1; // error
 	}
 	else
 	{
-		char* key = argv[1];
+		char* key = argv[1]; // get keyword from command-line
 		//printf("Enter text to encrypt: ");
-		char* plaintext = GetString();
+		char* plaintext = GetString(); // get plaintext from user
 
+		// this array will store the indexes of the plaintext
+		// i.e. if the plaintext is abc, this will hold 0, 1, 2
 		int* plain_indexes = malloc(sizeof(int) * strlen(plaintext));
 		for (int i = 0; i < strlen(plaintext); i++)
 		{
@@ -47,29 +52,33 @@ int main(int argc, char* argv[])
 				plain_indexes[i] = (plaintext[i] % 32) - 1;
 			}
 			else
-				plain_indexes[i] = 42;
+				// if the element in the string is not a letter
+				plain_indexes[i] = 42; // its index is 42 (>25)
 		}
 
+		// this array will store the indexes of the key
+		// i.e. if the key is abc, this will hold 0, 1, 2
 		int* key_indexes = malloc(sizeof(int) * strlen(key));
 		for (int i = 0; i < strlen(key); i++)
-			key_indexes[i] = (key[i] % 32) - 1;
+			key_indexes[i] = (key[i] % 32) - 1; // key is always all letters, because, if it wasn't, there would be errors
 
+		// this array will hold the chars of the cyphertext
 		char* cyphertext = malloc(sizeof(char) * strlen(plaintext));
 		for (int i = 0, j = 0; i < strlen(plaintext); i++)
 		{
-			if (plain_indexes[i] > 25)
-				cyphertext[i] = plaintext[i];
+			if (plain_indexes[i] > 25) // if the index is > 25
+				cyphertext[i] = plaintext[i]; // ignore it
 			else
 			{
-				if (isupper(plaintext[i]))
+				if (isupper(plaintext[i])) // if its uppercase
 				{
-					cyphertext[i] = UPALPHA[(plain_indexes[i] + key_indexes[j % strlen(key)]) % 26];
-					j++;
+					cyphertext[i] = UPALPHA[(plain_indexes[i] + key_indexes[j % strlen(key)]) % 26]; // add uppercase ((p_i + k_j) % 26) to the cyphertext
+					j++; // add 1 to the key incrementor
 				}
-				else
+				else // if its lowercase
 				{
-					cyphertext[i] = LOALPHA[(plain_indexes[i] + key_indexes[j % strlen(key)]) % 26];
-					j++;
+					cyphertext[i] = LOALPHA[(plain_indexes[i] + key_indexes[j % strlen(key)]) % 26]; // add lowercase ((p_i + k_j) % 26) to the cyphertext
+					j++; // add 1 to the key incrementor
 				}
 			}
 		}
